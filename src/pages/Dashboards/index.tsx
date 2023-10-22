2ff7cfd25193641af7fb2c2cf2ffbcc42f2c1ecd
Guilherme Andreta <guilherme.andretasantos@hotmail.com>
2023-10-22 20:36:09 -0300

    Inserido inputs de User e Senha
    Será iniciado a implementação da lógica para a verificação de usuário para confirmação correta de login, previsto para implementação até a data de 28/10

--- src/pages/Dashboards/index.tsx
+++ src/pages/Dashboards/index.tsx
@@ -1,31 +1,53 @@
 import React from "react";
-import { View, Button, StyleSheet } from "react-native";
+import { View, Button, StyleSheet, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import {useAuth} from "../../contexts/auth";
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-});
-
 const Dashboard: React.FC = () => {
-    const { signOut } = useAuth();
+    const { user, signOut } = useAuth();
 
-    //console.log(signed);
+    console.log(user);
 
     function handleSignOut() {
         signOut();
     }
 
     return(
-        <View style={styles.container}>
-            <Button title="Sign out" onPress={handleSignOut} />
-        </View>
+        <LinearGradient
+            colors={["#7BD2F6", "#946DED"]}
+            style={styles.container}
+        >
+            <Text style={styles.h1}>Folha de Pagamento</Text>
+
+            <View style={styles.container}>
+                <Button title="Sign out" onPress={handleSignOut} />
+            </View>
+        </LinearGradient>
     )
 }
 
-
-
 export default Dashboard;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        margin: 0,
+        padding: 0,
+        width: "100%",
+    },
+
+    h1: {
+        fontSize: 26,
+        fontWeight: "800",
+        fontStyle: "normal",
+        color: "#fff",
+        marginBottom: 30,
+        textShadowColor: "#000",
+        textShadowOffset: {width: 2, height: 2},
+        textShadowRadius: 3,
+    },
+
+});
--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,32 +1,125 @@
 import React from "react";
-import { View, Button, StyleSheet } from "react-native";
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    Platform,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    Keyboard,
+    Button
+} from "react-native";
+import {LinearGradient} from "expo-linear-gradient";
 
-import { signIn } from '../../services/auth';
-import {useAuth} from "../../contexts/auth";
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-});
+import { useAuth } from "../../contexts/auth";
 
 const SignIn: React.FC = () => {
-    const { signed, signIn } = useAuth();
+    const { signed, user, signIn } = useAuth();
 
-    //console.log(signed);
+    console.log(signed);
+    console.log(user);
 
     function handleSignIn() {
         signIn();
     }
 
-    return(
-        <View style={styles.container}>
-            <Button title="Sign in" onPress={handleSignIn} />
-        </View>
-    )
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <LinearGradient
+                colors={["#7BD2F6", "#946DED"]}
+                style={styles.container}
+            >
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    style={styles.container}
+                >
+                    <Text style={styles.h1}>Faça Seu Login</Text>
+
+                    <View style={styles.viewLoginSenha}>
+                        <Text style={styles.labelLoginSenha}>USER:</Text>
+
+                        <TextInput
+                            textContentType="nickname"
+                            style={styles.inputUserPass}
+                            placeholder="Digite seu Usuário"
+                        />
+                    </View>
+
+                    <View style={styles.viewLoginSenha}>
+                        <Text style={styles.labelLoginSenha}>PASS:</Text>
+
+                        <TextInput
+                            style={styles.inputUserPass}
+                            placeholder="Digite sua Senha"
+                            textContentType="password"
+                            secureTextEntry={true}
+                        />
+                    </View>
+
+            
+                    <Button title="Sign in" onPress={handleSignIn} />
+                    
+                </KeyboardAvoidingView>
+            </LinearGradient>
+        </TouchableWithoutFeedback>
+    );
 }
 
-
-
 export default SignIn;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        margin: 0,
+        padding: 0,
+        width: "100%",
+    },
+
+    h1: {
+        fontSize: 26,
+        fontWeight: "800",
+        fontStyle: "normal",
+        color: "#fff",
+        marginBottom: 30,
+        textShadowColor: "#000",
+        textShadowOffset: {width: 2, height: 2},
+        textShadowRadius: 3,
+    },
+
+    inputUserPass: {
+        color: "#fff",
+        fontSize: 18,
+        fontWeight: "500",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "80%",
+        paddingLeft: 15,
+        paddingTop: 5,
+        marginBottom: 15,
+        borderColor: "#fff",
+        borderWidth: 2,
+        borderRadius: 10,
+    },
+
+    viewLoginSenha: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "90%",
+    },
+
+    labelLoginSenha: {
+        fontSize: 18,
+        fontWeight: "600",
+        color: "#fff",
+        marginRight: 10,
+        textShadowColor: "#000",
+        textShadowOffset: {width: 2, height: 2},
+        textShadowRadius: 3,
+        paddingBottom: 13,
+    },
+
+});
