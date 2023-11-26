650580a1dab14cb7d29314b995d44f13b1028e5d
Guilherme Andreta <guilherme.andretasantos@hotmail.com>
2023-11-25 23:09:49 -0300

    Removido Axios e adicionado o fetch

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 export interface User {
     user: string;
     password: string;
@@ -11,64 +9,39 @@ export async function authenticateUser(
     password: string
 ): Promise<User | null> {
     try {
-        /*const pool = new ConnectionPool(dbConfig);
-        await pool.connect();
+        const APIurl = "http://localhost:5290/api/Authentication";
 
-        const result = await pool.query`
-            SELECT * FROM Users WHERE username = ${username} AND password = ${password}
-        `;
-
-        await pool.close();
-
-        if (username) {
-            // Usuário autenticado com sucesso
-            return {
-                user: username,
-                password: password,
-                token: username,
-            };
-        } else {
-            // Credenciais inválidas
-            return null;
-        }*/
-
-        // Fazer uma solicitação à API de autenticação
-
-        const apiUrl = 'http://127.0.0.1:5290/api/Authentication';
-
-        console.log(username, password);
-
-        const response = await axios.post(apiUrl, {
-            login: username,
-            senha: password,
+        const response = await fetch(APIurl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                login: username,
+                senha: password,
+            }),
         });
-        
-        console.log("Após a solicitação da API", response);
 
-        // Verificar a resposta da API
-        if (response.data.authenticated) {
-            // Usuário autenticado com sucesso
-            return {
-                user: username,
-                password: password,
-                token: response.data.token,
-            };
-            
+        if (response.ok) {
+            const data = await response.json();
+
+            if (data.authenticated) {
+                // Usuário autenticado com sucesso
+                return {
+                    user: username,
+                    password: password,
+                    token: data.token,
+                };
+            } else {
+                // Credenciais inválidas
+                return null;
+            }
         } else {
-            // Credenciais inválidas
+            console.error("Erro ao fazer a solicitação:", response.statusText);
             return null;
         }
     } catch (error) {
-        console.error("Erro ao fazer a solicitação:", error);
-
-        if (axios.isAxiosError(error)) {
-            console.error(
-                "Erro de autenticação:",
-                error.response?.data || error.message
-            );
-        } else {
-            console.error("Erro de autenticação:", error);
-        }
+        console.error("Erro de autenticação:", error);
         throw error;
     }
 }
